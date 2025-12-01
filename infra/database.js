7ece173478e0f6b347e63633a9f2dbf7eb32ec83
Pedro Cabeceira <109092210+pkbceira03@users.noreply.github.com>
2025-12-01 13:36:35 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-
-  console.log('Credencias do banco', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -34,3 +27,11 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues(){
+  if(process.env.POSTEGRES_CA){
+    return process.env.POSTGRES_CA;
+  }
+
+  return process.env.NODE_ENV === "development" ? false: true;
+}
