09f591696cfc7f452dca07a8dc1b5f0a529028ce
Pedro Cabeceira <109092210+pkbceira03@users.noreply.github.com>
2025-12-08 15:01:20 +0000

    fix `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,37 +3,49 @@ import { join } from 'node:path';
 import database from 'infra/database.js';
 
 export default async function migrations(req, res) {
-  const dbClient = await database.getNewClient();
-  
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations"
-  };
-
-  if(req.method === "GET"){
-    const pendingMigrations = await migrationsRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return res.status(200).json(pendingMigrations);
+  const allowedMethods = ["GET", "POST"];
+  if(!allowedMethods.includes(req.method)){
+    return res.status(405).json({
+      error: `Method "${req.method}"not allowed`
+    }); 
   }
 
-  if(req.method === "POST"){
-    const migrateMigrations = await migrationsRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
+  let dbClient;
+  try{
+    dbClient = await database.getNewClient();
     
-    if(migrateMigrations.length > 0){
-      return res.status(201).json(migrateMigrations);
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations"
+    };
+
+    if(req.method === "GET"){
+      const pendingMigrations = await migrationsRunner(defaultMigrationOptions);
+      return res.status(200).json(pendingMigrations);
     }
 
-    return res.status(200).json(migrateMigrations);
+    if(req.method === "POST"){
+      const migrateMigrations = await migrationsRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+    
+      if(migrateMigrations.length > 0){
+        return res.status(201).json(migrateMigrations);
+      }
+
+      return res.status(200).json(migrateMigrations);
+    }
+    
+  }catch (error) {
+    console.error(error);
+    throw error;
+  }finally {
+    await dbClient.end();
   }
-  
-  return res.status(405).end();
 }
 
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -13,7 +13,7 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(response1.status).toBe(201);
 
   const response1Body = await response1.json();
-  console.log(response1Body);
+  //console.log(response1Body);
   expect(Array.isArray(response1Body)).toBe(true);
   expect(response1Body.length).toBeGreaterThan(0);
   expect(response1Body[0].path).toBe("infra/migrations/1764769376103_test-migration.js")
@@ -24,7 +24,7 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(response2.status).toBe(200);
 
   const response2Body = await response2.json();
-  console.log(response2Body);
+  //console.log(response2Body);
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
 
