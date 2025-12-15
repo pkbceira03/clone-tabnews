d9e292f9a3bb4d8c23d40910d2bf0a3ea8610912
Pedro Cabeceira <109092210+pkbceira03@users.noreply.github.com>
2025-12-15 13:51:08 +0000

    fix `prettier` style linting

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,15 +5,14 @@ async function waitForAllServices() {
 
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
-      retries:100,
+      retries: 100,
       maxTimeout: 1000,
     });
 
-    async function fetchStatusPage() { 
+    async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
       if (response.status !== 200) {
         throw Error();
-        
       }
     }
   }
@@ -21,4 +20,4 @@ async function waitForAllServices() {
 
 export default {
   waitForAllServices,
-}
+};
