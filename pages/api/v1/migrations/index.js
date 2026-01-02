8b2a3288cf097bac32271abd4b215e51ff771e87
Pedro Cabeceira <109092210+pkbceira03@users.noreply.github.com>
2026-01-02 14:48:36 +0000

    refactor: make `/api/v1/migrations` use `migrator` model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,57 +1,24 @@
 import { createRouter } from "next-connect";
-import migrationsRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
-import controller from "infra/controller";
+import controller from "infra/controller.js";
 const router = createRouter();
+import migrator from "models/migrator.js";
 
 router.get(getHandler);
 router.post(postHandler);
 
 export default router.handler(controller.errorHandler);
 
-const defaultMigrationOptions = {
-  dryRun: true,
-  dir: resolve("infra", "migrations"),
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
-
 async function getHandler(req, res) {
-  let dbClient;
-
-  try {
-    dbClient = await database.getNewClient();
-
-    const pendingMigrations = await migrationsRunner({
-      ...defaultMigrationOptions,
-      dbClient,
-    });
-    return res.status(200).json(pendingMigrations);
-  } finally {
-    await dbClient.end();
-  }
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return res.status(200).json(pendingMigrations);
 }
 
 async function postHandler(req, res) {
-  let dbClient;
+  const migrateMigrations = await migrator.runPendingMigrations();
 
-  try {
-    dbClient = await database.getNewClient();
-
-    const migrateMigrations = await migrationsRunner({
-      ...defaultMigrationOptions,
-      dbClient,
-      dryRun: false,
-    });
-
-    if (migrateMigrations.length > 0) {
-      return res.status(201).json(migrateMigrations);
-    }
-
-    return res.status(200).json(migrateMigrations);
-  } finally {
-    await dbClient.end();
+  if (migrateMigrations.length > 0) {
+    return res.status(201).json(migrateMigrations);
   }
+
+  return res.status(200).json(migrateMigrations);
 }
